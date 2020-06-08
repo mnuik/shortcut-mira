e972d79b6ce5b2f35ee7a287f64c3e82a811d4d6
mnuik <63105966+mnuik@users.noreply.github.com>
2020-06-08 16:20:15 +0300

    My FullStack projects
    Own website project

---
+++ Interactive Test 1 (hedgehog)/functions.js
@@ -0,0 +1,12 @@
+//let imgOne = "https://pbs.twimg.com/profile_images/3566759842/800940c7ec3bd6ad39fca4eccbb877e7_400x400.jpeg"
+function imageSwap() {
+    document.getElementById("imgOne").src = "https://k31.kn3.net/taringa/1/B/8/2/D/B/lecheenpolvo23/97D.jpg";
+    
+
+}
+
+function imageSwap2() {
+    document.getElementById("imgOne").src = "https://i.pinimg.com/originals/d0/09/66/d00966ef52bb95177169a6883e90a05e.jpg";
+
+} 
+//https://pbs.twimg.com/profile_images/3566759842/800940c7ec3bd6ad39fca4eccbb877e7_400x400.jpeg
